692d1e029b2216c96e986162df6bd412e0b883b5
homesinfra <49340002+homesinfra@users.noreply.github.com>
2020-07-10 11:03:10 +0530

    1

--- profileUpload.php
+++ profileUpload.php
@@ -12,10 +12,52 @@ if($_FILES["file"]["name"] != '')
  //  $name = rand(100, 999) . '.' . $ext;
   $location = 'assets/images/profile_pics/';  
   $imagePath = $location . uniqid() . basename($imageName);
-move_uploaded_file($_FILES["file"]["tmp_name"], $imagePath);
  
+ move_uploaded_file($_FILES['file']['tmp_name'], $imagePath);
  echo $imagePath;
- 
 $update_query1 = mysqli_query($con, "UPDATE users SET profile_pic='$imagePath' WHERE username='$userLoggedIn'");
 }
+
+
+//  // Getting file name
+//  $filename = $_FILES['imagefile']['name'];
+ 
+//  // Valid extension
+//  $valid_ext = array('png','jpeg','jpg');
+
+//  // Location
+//  $location = "assets/images/profile_pics/".$filename;
+
+//  // file extension
+//  $file_extension = pathinfo($location, PATHINFO_EXTENSION);
+//  $file_extension = strtolower($file_extension);
+
+//  // Check extension
+//  if(in_array($file_extension,$valid_ext)){
+
+//    // Compress Image
+//    compressImage($_FILES['imagefile']['tmp_name'],$location,60);
+
+//  }else{
+//    echo "Invalid file type.";
+//  }
+// }
+
+// // Compress image
+// function compressImage($source, $destination, $quality) {
+
+//  $info = getimagesize($source);
+
+//  if ($info['mime'] == 'image/jpeg') 
+//    $image = imagecreatefromjpeg($source);
+
+//  elseif ($info['mime'] == 'image/gif') 
+//    $image = imagecreatefromgif($source);
+
+//  elseif ($info['mime'] == 'image/png') 
+//    $image = imagecreatefrompng($source);
+
+//  imagejpeg($image, $destination, $quality);
+
+// }
 ?>
